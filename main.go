25df884defddc84ee160ea7eb65db2049934a6f2
Anderson Nascimento <anderson.nascimento@meta.com.br>
2022-12-02 14:38:47 -0300

    variables and fun in Golang

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/AndersonNascimentoAFSN/golang-do-zero/math"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	//// variables
+	// var name string
+	// name = "Anderson Nascimento"
+	// age, city := 31, "Maceió"
+	// state := "Alagoas"
+
+	// fmt.Printf("%v\n%v\n%v\n%v\n", name, age, city, state)
+	// fmt.Printf("%T\n%T\n%T\n%T\n", name, age, city, state)
+
+	total := math.Sum(1, 2)
+	fmt.Printf("%T\n", total)
 }
---
+++ math/operations.go
@@ -0,0 +1,6 @@
+package math
+
+// Sum é uma função que recebe dois números inteiros e retorna a soma deles
+func Sum(a int, b int) int {
+	return a + b
+}
